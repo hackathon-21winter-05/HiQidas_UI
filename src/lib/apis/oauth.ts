8298b2415e28e9db78ff55a3a6475c5b5016ce73
reyu <ka15sugar@gmail.com>
2021-12-10 09:53:20 +0900

    fix eslintignore

--- src/lib/apis/oauth.ts
+++ src/lib/apis/oauth.ts
@@ -1,17 +1,19 @@
-import {oauth} from "/@/lib/apis/pb/rest/oauth"
-import axios from "axios"
+import { oauth } from '/@/lib/apis/pb/rest/oauth'
+import axios from 'axios'
 
 export const getOAuthCallback = async () => {
-	const res = await axios.get("/api/oauth/callback", {responseType: "arraybuffer"})
-	
-	const data = oauth.GetOauthCallbackResponse.decode(new Uint8Array(res.data))
+  const res = await axios.get('/api/oauth/callback', {
+    responseType: 'arraybuffer',
+  })
 
-	return data
+  const data = oauth.GetOauthCallbackResponse.decode(new Uint8Array(res.data))
+
+  return data
 }
 
-export const postOAuthCode = async (code: string) => { 
-	const req = oauth.PostOauthCodeRequest.create({ code })
-	const buffer = oauth.PostOauthCodeRequest.encode(req).finish()
+export const postOAuthCode = async (code: string) => {
+  const req = oauth.PostOauthCodeRequest.create({ code })
+  const buffer = oauth.PostOauthCodeRequest.encode(req).finish()
 
-	await axios.post("/api/oauth/code", new Uint8Array(buffer))
+  await axios.post('/api/oauth/code', new Uint8Array(buffer))
 }
