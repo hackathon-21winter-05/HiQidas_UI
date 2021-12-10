d01fb2f8cae27630850dd1405b8031f93373863b
Rozelin <rozelin.dc@gmail.com>
2021-12-10 22:40:03 +0900

    editHeyaData

--- src/lib/apis/heyas.ts
+++ src/lib/apis/heyas.ts
@@ -16,5 +16,23 @@ export const createHeya = async (title: string, description: string) => {
   })
   const buffer = heya.PostHeyasRequest.encode(req).finish()
 
-  await axios.post('/api/heyas', new Uint8Array(buffer))
+  const res = await axios.post('/api/heyas', new Uint8Array(buffer))
+
+  const data = heya.PostHeyasResponse.decode(new Uint8Array(res.data))
+
+  return data
+}
+
+export const editHeyaData = async (
+  heyaId: string,
+  newTitle: string,
+  newDescription: string
+) => {
+  const req = heya.PutHeyasHeyaIdRequest.create({
+    title: newTitle,
+    description: newDescription,
+  })
+  const buffer = heya.PutHeyasHeyaIdRequest.encode(req).finish()
+
+  await axios.put(`/api/heyas/${heyaId}`, new Uint8Array(buffer))
 }
