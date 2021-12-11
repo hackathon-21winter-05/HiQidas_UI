a9f6b187b3328db704f92cf01162b75e8099f11d
Rozelin <rozelin.dc@gmail.com>
2021-12-11 23:28:34 +0900

    fix

--- src/permission.ts
+++ src/permission.ts
@@ -1,30 +1,28 @@
+import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import router from '/@/router'
-import { useMe } from '/@/providers/me'
 
 router.beforeEach(async (to, _, next) => {
   if (to.meta && to.meta?.isPublic) {
     // 未ログインでも閲覧可なページ
     next()
   } else {
-    try {
-      await useMe()
-      next()
-    } catch (error) {
-      if (error === 'Not logged in.') {
-        ElMessage({
-          message: `ログインしてください`,
-          type: 'error',
-        })
-        // 未ログインの場合、ログインページにリダイレクト
-        next(`/login?redirect=${to.path}`)
-      } else {
-        ElMessage({
-          message: `エラーが発生しました\n${error}`,
-          type: 'error',
-        })
-        console.log(error)
-      }
+    const res = await axios.get('/api/users/me', {
+      responseType: 'arraybuffer',
+    })
+    if (res.status === 401) {
+      ElMessage({
+        message: `ログインしてください`,
+        type: 'error',
+      })
+      // 未ログインの場合、ログインページにリダイレクト
+      next(`/login?redirect=${to.path}`)
+    } else {
+      ElMessage({
+        message: `エラーが発生しました\n${res}`,
+        type: 'error',
+      })
+      console.log(res)
     }
   }
 })
