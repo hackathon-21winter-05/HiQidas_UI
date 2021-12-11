e29808540eb6ed8f60bb1a5eff191c4fa7101809
Rozelin <rozelin.dc@gmail.com>
2021-12-11 22:35:39 +0900

    コンフリクト解消

--- src/lib/apis/users.ts
+++ src/lib/apis/users.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { GetUsersMeResponse } from '/@/lib/pb/protobuf/rest/users'
+import * as users from '/@/lib/pb/protobuf/rest/users'
 
 export const getMe = async () => {
   const res = await axios.get('/api/users/me', {
@@ -7,5 +7,14 @@ export const getMe = async () => {
   })
   if (res.status !== 200) throw new Error(res.statusText)
 
-  return GetUsersMeResponse.decode(new Uint8Array(res.data)).me
+  return users.GetUsersMeResponse.decode(new Uint8Array(res.data)).me
+}
+
+export const createUser = async (name: string) => {
+  const req = users.PostUsersRequest.fromJSON({ name: name })
+  const buffer = users.PostUsersRequest.encode(req).finish()
+
+  await axios.post('/api/users', new Uint8Array(buffer), {
+    responseType: 'arraybuffer',
+  })
 }
