2c49057ea89522063c5ff7668cdef94237fd0af8
Rozelin <rozelin.dc@gmail.com>
2021-12-11 17:34:56 +0900

    Update index.vue

--- src/views/LoginPage/index.vue
+++ src/views/LoginPage/index.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="login-container">
     <h1 class="title">HiQidas</h1>
+    <el-form ref="loginForm" :model="loginForm" label-position="top">
+      <el-form-item label="ユーザーネーム">
+        <el-input v-model="loginForm.name" />
+      </el-form-item>
+      <el-form-item label="パスワード">
+        <el-input v-model="loginForm.password" />
+      </el-form-item>
+      <el-button @click="login">ログイン</el-button>
+    </el-form>
+
+    <el-button @click="loginWithPortal">traPortalでログイン</el-button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 
 export default defineComponent({
   name: 'LoginPage',
+  setup() {
+    const loginForm = reactive({ name: '', password: '' })
+
+    const login = () => {
+      // TODO: ログインする
+      console.log('logged in!', loginForm)
+    }
+
+    const loginWithPortal = () => {
+      location.href = `https://portal.trap.jp/login?redirect=${location.host}`
+    }
+
+    return { loginForm, login, loginWithPortal }
+  },
 })
 </script>
+
+<style lang="scss" scoped>
+.login-container {
+  margin: 0 auto;
+  width: 500px;
+  max-width: 90%;
+}
+</style>
